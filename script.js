e71535b74df668cd3f069bc710746bb26b53a169
git push -u origin main <tinitosguerrero@gmail.com>
2024-11-26 12:22:02 -0500

    Modificaciones varias

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ document.addEventListener('DOMContentLoaded', function() {
     botonContainer.style.display = 'none';
 });
 
-// Evento: Mostrar el botón al finalizar el video
+// Mostrar el botón al finalizar el video
 video.addEventListener('ended', function() {
     botonContainer.style.display = 'flex'; // Muestra el contenedor al terminar el video
 });
 
 
-// Función: Navegar a la siguiente vista
+// Navegar a la siguiente vista
 function irASiguienteVista() {
     // Redirige a la nueva página
     window.location.href = "./vista/felicidades.html";
 
-    // Elimina el historial actual para que no se pueda volver
+    // Elimina el historial actual para que no se pueda volver a la ventana anterior
     //window.history.replaceState(null, '', './vista/felicidades.html');
 }
 
 function cerrarVentana() {
-    alert("Por favor, cierra el navegador manualmente para finalizar.");
+    alert("DEBES CERRAR EL NAVEGADOR MANUALMENTE")
 }
